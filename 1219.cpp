890d057aa8aedb8bf186cbd3a5682f1fd0640fc0
heize <sonsy0929@naver.com>
2019-09-08 23:41:05 +0900

    오민식의 고민

---
+++ 1219.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+using ll = long long;
+using p = pair<int,int>;
+const ll INF = 1e18;
+vector<p> adj[100];
+int N, S, E, M, income[100];
+int isConnect[100][100];
+void floyd(){
+    for (int k = 0; k < N; k++){
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                if (isConnect[i][j] > isConnect[i][k] + isConnect[k][j]){
+                    isConnect[i][j] = isConnect[i][k] + isConnect[k][j];
+                }
+            }
+        }
+    }
+}
+int main(){
+    scanf("%d %d %d %d", &N, &S, &E, &M);
+    for (int i = 0; i < N; i++) fill(isConnect[i], isConnect[i] + N, 1e9);
+    for (int i = 0; i < M; i++){
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        adj[u].push_back({v,w});
+        isConnect[u][v] = 1;
+    }
+    for (int i = 0; i < N; i++){
+        scanf("%d", &income[i]);
+        isConnect[i][i] = 1;
+    }
+    ll money[100];
+    fill(money, money + 100, INF);
+    money[S] = -income[S];
+    bool GEE = false;
+    floyd();
+    for (int i = 0; i < M; i++){
+        for (int j = 0; j < N; j++){
+            for (auto pNext : adj[j]){
+                int next = pNext.first;
+                int w = pNext.second;
+                if (money[j] == INF) continue;
+                if (money[next] > money[j] + w - income[next]){
+                    money[next] = money[j] + w - income[next];
+                    if (i == M - 1){
+                        GEE |= (isConnect[j][E] != 1e9);
+                    }
+                }
+            }
+        }
+    }
+    if (money[E] == INF) puts("gg");
+    else {
+        if (!GEE) printf("%lld\n", -money[E]);
+        else puts("Gee");
+    }
+}
