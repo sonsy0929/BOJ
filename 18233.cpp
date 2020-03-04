b74c52240d592f0a53029b098711a6bc4bab009e
heize <sonsy0929@naver.com>
2020-03-04 14:29:34 +0900

    러버덕을 사랑하는 모임

--- 18233.cpp
+++ 18233.cpp
@@ -1,56 +1,39 @@
-#include <cstdio>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-using p = pair<int, int>;
-int N, P, E;
-vector<int> v;
-vector<p> boundaryVector;
-bool isPossible(){
-    int mn = 0, mx = 0;
-    for (int i : v){
-        mn += boundaryVector[i].first;
-        mx += boundaryVector[i].second;
-    }
-    return mn <= E && E <= mx;
-}
-bool comb(int pos, int cnt, vector<int> &ansList){
-    if (cnt == P){
-        if (isPossible()){
-            int sum = 0;
-            ansList.resize(N);
-            for (int i : v){
-                sum += boundaryVector[i].first;
-                ansList[i] = boundaryVector[i].first;
-            }
-            if (sum < E){
-                for (int i : v){
-                    int diff = boundaryVector[i].second - boundaryVector[i].first;
-                    if (E - sum <= 0) break; 
-                    int mn = min(E - sum, diff);
-                    ansList[i] += mn;
-                    sum += mn; 
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-    for (int i = pos; i < N; i++){
-        v.push_back(i);
-        if (comb(i + 1, cnt + 1, ansList)) return true;
-        v.pop_back();
-    }
-    return false;
-}
-int main(){
+int N, P, E, x[20], y[20];
+int main() {
     scanf("%d %d %d", &N, &P, &E);
-    for (int i = 0; i < N; i++){
-        int x, y;
-        scanf("%d %d", &x, &y);
-        boundaryVector.push_back({x, y}); 
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", x + i, y + i);
     }
-    vector<int> ansList;
-    bool possible = comb(0, 0, ansList);
-    if (!possible) puts("-1");
-    else for (int ans : ansList) printf("%d ", ans);
+    vector<int> ans;
+    for (int b = 1; b < (1 << N); b++) {
+        if (__builtin_popcount(b) != P) continue;
+        vector<int> tracking(N, 0);
+        int sum = 0, xSum = 0, ySum = 0;
+        for (int i = 0; i < N; i++) {
+            if (b & (1 << i)) {
+                tracking[i] += x[i];
+                sum += x[i];
+                xSum += x[i], ySum += y[i];
+            }
+        }
+        if (xSum <= E && E <= ySum) {
+            for (int i = 0; i < N; i++) {
+                if (!tracking[i]) continue;
+                int add = min(y[i] - x[i], E - sum);
+                tracking[i] += add;
+                sum += add;
+            }
+            ans = tracking;
+            break;
+        }
+    }
+    if (ans.empty()) {
+        puts("-1");
+    } else {
+        for (int curr : ans) {
+            printf("%d ", curr);
+        }
+    }
 }
